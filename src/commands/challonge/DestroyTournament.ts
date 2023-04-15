5698e444d310b8d3e088cb2ba4002c0136e131ba
arcetros <75669794+arcetros@users.noreply.github.com>
2023-04-15 22:55:15 +0800

    feat(commands/challonge): add `tcreate`, `tinfo`, `tdestroy` commands

---
+++ src/commands/challonge/DestroyTournament.ts
@@ -0,0 +1,41 @@
+import Commands from "../../structures/Commands";
+import Challonge from "../../api/Client";
+import Bot from "../../structures/Bot";
+import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+export default class DestroyTournament extends Commands {
+    public challonge: Challonge;
+    constructor(public client: Bot) {
+        super(client);
+        this.challonge = new Challonge({ api_key: process.env.CHALLONGE_API_KEY as string });
+    }
+    name = "tdestroy";
+    visible = true;
+    description = "Delete a tournament";
+    information = this.description;
+    aliases = [];
+    args = false;
+    usage = "";
+    example = "";
+    cooldown = 10;
+    category = "challonge";
+    guildOnly = false;
+    data = new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription(this.description)
+        .addStringOption(option =>
+            option
+                .setName("tournament_id")
+                .setDescription("The ID of the tournament to process the action")
+                .setRequired(true)
+        );
+    execute = async (interaction: CommandInteraction<CacheType>): Promise<void> => {
+        const tournament_id = interaction.options.get("tournament_id")?.value as string;
+        try {
+            const response = await this.challonge.tournament.destroy(tournament_id);
+            await interaction.reply({ content: `${response.name} is successfully destroyed` });
+        } catch (err) {
+            interaction.reply({ content: `${err}` });
+        }
+    };
+}
