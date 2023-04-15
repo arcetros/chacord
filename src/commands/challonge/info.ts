bd4104d263a45a4c80217c9a1447052c9c3000b5
arcetros <75669794+arcetros@users.noreply.github.com>
2023-04-15 19:38:55 +0800

    feat(commands/challonge): add show tournament

---
+++ src/commands/challonge/info.ts
@@ -0,0 +1,89 @@
+import { CommandInteraction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { ITournament } from "../../typings";
+import Challonge from "../../api/Client";
+import Commands from "../../structures/Commands";
+import Bot from "../../structures/Bot";
+
+export default class Info extends Commands {
+    constructor(public client: Bot) {
+        super(client);
+    }
+    public challonge = new Challonge({ api_key: process.env.CHALLONGE_API_KEY as string });
+    name = "info";
+    visible = true;
+    description = "View important detail about a tournament";
+    information = this.description;
+    aliases = [];
+    args = false;
+    usage = "[tournament name]";
+    example = "";
+    cooldown = 10;
+    category = "challonge";
+    guildOnly = false;
+    data = new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription(this.description)
+        .addStringOption(option =>
+            option
+                .setName("tournament_id")
+                .setDescription("The ID of the tournament to process the action")
+                .setRequired(true)
+        );
+    execute = async (interaction: CommandInteraction): Promise<void> => {
+        const tournament_id = interaction.options.get("tournament_id")?.value as string;
+        const response = await this.challonge.tournament.show(tournament_id, true);
+
+        const startDate = response.start_at ? new Date(response.start_at).toLocaleString() : null;
+
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: `Challonge Bot`,
+                url: "https://github.com/arcetros/chacord",
+                iconURL: "https://raw.githubusercontent.com/arcetros/chacord/main/assets/challonge_icon.png"
+            })
+            .setTitle(`${response.name} - [ID: ${tournament_id}]`)
+            .setURL(response.full_challonge_url)
+            .addFields(
+                {
+                    name: "Start Time",
+                    value: startDate ? startDate : "Unknown"
+                },
+                {
+                    name: "Current Participants",
+                    value: `${response.participants_count} Players`,
+                    inline: true
+                },
+                {
+                    name: "Max Participants",
+                    value: `${response.signup_cap ? response.signup_cap : "∞"} Players`,
+                    inline: true
+                },
+                {
+                    name: "Participants",
+                    value: response.participants.map(participant => `> ${participant.name}`).join("\n")
+                }
+            )
+            .setImage("https://challonge.com/askjdfhalsdkfha.svg")
+            .setColor("#ed9645")
+            .setFooter({
+                text: `STATUS: ${this.getEmojiForState(response.state)}`
+            });
+
+        interaction.reply({ embeds: [embed] });
+    };
+
+    private getEmojiForState(state: ITournament["state"]): string {
+        switch (state) {
+            case "pending":
+                return "⏰ Pending"; // hourglass emoji
+            case "underway":
+                return "▶️ In progress"; // fire emoji
+            case "awaiting_review":
+                return "🤔 Waiting for reviews"; // thinking face emoji
+            case "complete":
+                return "✅ Complete"; // party popper emoji
+            default:
+                return "❌ State is unknown";
+        }
+    }
+}
